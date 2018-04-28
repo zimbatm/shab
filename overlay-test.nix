7e271969daeee1306d9775a69faf551272d55c56
Robert Hensing <robert@roberthensing.nl>
2018-04-28 01:06:15 +0100

    Simplify runShab, add runShabText, add docs

--- overlay-test.nix
+++ overlay-test.nix
@@ -1,13 +1,34 @@
 # Tests the overlay
 let
   pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
-  shabExample = pkgs.runShab "example" (pkgs.writeText "tpl" ''
+
+  shabExample = pkgs.runShab (pkgs.writeText "tpl" ''
     foo $hello bar
   '') { hello = 123; } ;
-in pkgs.runCommand "test" {
-     actual = shabExample;
-     expected = pkgs.writeText "expected" "foo 123 bar\n\n";
-   } ''
-     diff $actual $expected
-     :>$out
-   ''
+
+  shabTextExample = pkgs.runShabText ''
+    foo $hello bar
+  '' { hello = 123; } ;
+
+in
+{
+
+  test_runShab =
+    pkgs.runCommand "test" {
+      actual = shabExample;
+      expected = pkgs.writeText "expected" "foo 123 bar\n\n";
+    } ''
+      diff $actual $expected
+      :>$out
+    '';
+
+  test_runShabText =
+    pkgs.runCommand "test" {
+      actual = shabTextExample;
+      expected = pkgs.writeText "expected" "foo 123 bar\n\n";
+    } ''
+      diff $actual $expected
+      :>$out
+    '';
+
+}
